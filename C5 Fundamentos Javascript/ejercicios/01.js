1883ed5e6f683cd3bc714cad474844136466225d
Jack <jshignotus@gmail.com>
2024-09-03 18:54:29 -0500

    First Homework

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,6 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = "CUYMALU";
+const nuevoString = "CUYMALUMILI";
 
 module.exports = nuevoString;
--- C5 Fundamentos Javascript/ejercicios/02.js
+++ C5 Fundamentos Javascript/ejercicios/02.js
@@ -1,6 +1,6 @@
 // Crea una variable de tipo number.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoNumero = 369;
+const nuevoNumero = 333;
 
 module.exports = nuevoNumero;
